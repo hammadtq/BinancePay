32087d1f9c1f20c7cda862e32563ae97d525be12
Hammad Tariq <hammadtariq@yahoo.co.uk>
2019-05-17 19:49:53 -0400

    Initial Commit

---
+++ BinancePay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BinancePay
+//
+//  Created by Hammad Tariq on 17/05/2019.
+//  Copyright © 2019 Hammad Tariq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
