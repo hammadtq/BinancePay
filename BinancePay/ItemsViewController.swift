d0fedaf8b854ed642fc90e77cba6336bfa7b5288
Hammad Tariq <hammadtariq@yahoo.co.uk>
2019-05-18 21:53:29 -0400

    Initial commit

---
+++ BinancePay/ItemsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ItemsViewController.swift
+//  BinancePay
+//
+//  Created by Hammad Tariq on 18/05/2019.
+//  Copyright © 2019 Hammad Tariq. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pods/Target Support Files/Pods-BinancePay/Pods-BinancePay-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BinancePay : NSObject
+@end
+@implementation PodsDummy_Pods_BinancePay
+@end
